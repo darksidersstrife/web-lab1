45974e86a676ac303cdce4506d3eebd3334e9336
darksidersstrife <vvllaadd99@yandex.ru>
2019-11-05 21:54:36 +0300

    make code react to submit event

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var weatherTemplate = doT.template(document.getElementById('weatherTemplate').text);
-var cityNotFoundTemplate = doT.template(document.getElementById('cityNotFoundTemplate').text);
-var errorTemplate = doT.template(document.getElementById('errorTemplate').text);
+const weatherTemplate = doT.template(document.getElementById('weatherTemplate').text);
+const cityNotFoundTemplate = doT.template(document.getElementById('cityNotFoundTemplate').text);
+const errorTemplate = doT.template(document.getElementById('errorTemplate').text);
 
-function getWeather() {
-    var content = document.getElementById('weather');
-    var cityName = document.getElementById('city').value;
+function getWeather(event) {
+    let content = document.getElementById('weather');
+    event.preventDefault();
     if (cityName === "") {
         return
     }
-    var request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
     request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?units=metric&appid=743425b4595f21f19d34ea81bf8f2f5c&q=' + cityName, false);
     request.send();
     if (request.status === 200) {
         response = JSON.parse(request.responseText);
-        var weather = {
+        const weather = {
             city: response.name,
             temp: response.main.temp,
             minTemp: response.main.temp_min,
