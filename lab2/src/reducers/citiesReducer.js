08122e40c3bcb24752f34d56c510ada95cb54f2b
darksidersstrife <vvllaadd99@yandex.ru>
2019-12-04 03:14:56 +0300

    fix update, add downloading state

--- lab2/src/reducers/citiesReducer.js
+++ lab2/src/reducers/citiesReducer.js
@@ -3,23 +3,19 @@
 
 export default function (cities, action) {
     let newState;
-    console.log(cities);
     console.log(action);
     switch (action.type) {
         case "ADD_CITY":
             console.log('add');
             return {...cities, [action.cityName] : null};
-        case "REMOVE_CITY":
+        case "DELETE_CITY":
             console.log('remove');
             newState = {...cities};
             delete newState[action.cityName];
             return newState;
         case "UPDATE_CITY":
-            console.log('update');
             newState = {...cities};
             newState[action.cityName] = action.cityInfo;
-            console.log(newState);
-            console.log(action);
             return newState;
         default:
             return cities
