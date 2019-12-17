a26644bcdb3cde7398418ac91bfa0e419000d259
darksidersstrife <vvllaadd99@yandex.ru>
2019-12-17 09:22:19 +0300

    add test starting code

---
+++ lab1/test/test.js
@@ -0,0 +1,16 @@
+
+describe('WeatherTests', function() {
+
+    before(function () {
+
+    });
+
+    describe('RenderTests', function () {
+        it('should print moscow temp', function () {
+
+        });
+    });
+
+    mocha.run();
+
+});
