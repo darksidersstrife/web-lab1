d816be4cf40b9a37e302d0180829875ee006ae2d
darksidersstrife <vvllaadd99@yandex.ru>
2019-12-18 07:29:07 +0300

    fix store

--- lab4/front/src/store/store.js
+++ lab4/front/src/store/store.js
@@ -2,6 +2,6 @@ import {applyMiddleware, createStore} from "redux";
 import reducer from "../reducers/reducer"
 import reduxpromise from "redux-promise"
 
-let store = createStore(reducer, {}, applyMiddleware(reduxpromise));
+let store = createStore(reducer, { cities : {}}, applyMiddleware(reduxpromise));
 
 export default store
