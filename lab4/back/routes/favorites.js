e63f12b84e5db9923dc5eb51875594019067623d
darksidersstrife <vvllaadd99@yandex.ru>
2019-12-20 23:05:44 +0300

    fix typos and set meaningful http codes in backend

--- lab4/back/routes/favorites.js
+++ lab4/back/routes/favorites.js
@@ -1,14 +1,13 @@
 var db = require('../db');
-var conf = require('../config');
-var fetch = require('node-fetch');
 
 module.exports = function (router) {
     router.get('/favorite', function (req, res) {
         db
-            .then(c => c.find({},  {projection:{_id: 0}}))
+            .then(c => c.find({}, {projection: {_id: 0}}))
             .then(res => res.toArray())
             .then(docs => res.send(JSON.stringify(docs)))
-            .catch(err => red.status(500).send(err.toString()))
+            .catch(err => req.status(500).send(err.toString()))
+            .catch(err => console.log(err.toString()))
     });
 
     router.put('/favorite', function (req, res) {
@@ -19,22 +18,40 @@ module.exports = function (router) {
 
         req.body.cityName = req.body.cityName.toLowerCase();
         db
-            .then(weather => db.then(c => c.updateOne({cityName : req.body.cityName}, { "$set" : req.body })))
-            .then(() => res.send("ok"))
-            .catch(err => res.status(500).send(err.toString()))
+            .then(c => c.updateOne({cityName: req.body.cityName}, {"$set": req.body}))
+            .then((result) => {
+                if (result.matchedCount === 0)
+                    throw "City not found";
+                else
+                    res.send("ok");
+            })
+            .catch(err => {
+                if (err === "City not found")
+                    res.status(404).send(err);
+                else
+                    res.status(500).send(err.toString());
+            })
+            .catch(err => console.log(err.toString()))
     });
 
     router.post('/favorite', function (req, res) {
 
         if (!req.body || !req.body.cityName) {
             res.status(400).send('City not provided');
+            return
         }
 
         req.body.cityName = req.body.cityName.toLowerCase();
         db
             .then(c => c.insertOne(req.body))
             .then(() => res.send("ok"))
-            .catch(err => res.status(500).send(err.toString()))
+            .catch(err => {
+                if (err.code === 11000)
+                    res.status(409).send("City is already added");
+                else
+                    res.status(500).send(err.toString());
+            })
+            .catch(err => console.log(err.toString()))
     });
 
     router.delete('/favorite', function (req, res) {
@@ -44,9 +61,19 @@ module.exports = function (router) {
         }
 
         db
-            .then(weather => db.then(c => c.deleteOne({cityName : req.body.name.toLowerCase()})))
-            .then(res => { if (res.deletedCount === 0) throw "City not found"; else return null })
-            .then(() => res.send("ok"))
-            .catch(err => res.status(500).send(err.toString()))
+            .then(c => c.deleteOne({cityName: req.body.name.toLowerCase()}))
+            .then(result => {
+                if (result.deletedCount === 0)
+                    throw "City not found";
+                else
+                    res.send("ok");
+            })
+            .catch(err => {
+                if (err === "City not found")
+                    res.status(404).send(err);
+                else
+                    res.status(500).send(err.toString());
+            })
+            .catch(err => console.log(err.toString()))
     });
 };
